d09ea54962ae077a2ce4a0f60150a62e377c95ea
juriilmjarv <juura.ilmjarv@gmail.com>
2020-05-18 10:48:29 +0100

    Routes

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,25 +86,16 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Juri Ilmjarv',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '11913',
-                    country: 'United Kingdom'
-                }
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order).then(response => {
-            this.setState({loading:false, purchasing: false});
-        }).catch(error => {
-            this.setState({loading: false, purchasing: false});
-        });
+       const queryParams = [];
+       for(let i in this.state.ingredients){
+           queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+       }
+       queryParams.push('price=' + this.state.totalPrice);
+       const queryString = queryParams.join('&');
+       this.props.history.push({
+           pathname: '/checkout',
+           search: '?' + queryString
+       });
     }
 
     render(){
